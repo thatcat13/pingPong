64dad391f4373a19f9aee1af6468004d41cebe1f
cj <thatcat13@gmail.com>
2017-12-28 12:48:28 -0800

    revisit pingPong repo and node lesson

--- js/pingpong-interface.js
+++ js/pingpong-interface.js
@@ -1,16 +1,20 @@
 //frontend
-var Whatevs = require("./../js/sillyBackEndFile.js").thingy; //specify which
+var Whatevs = require("./../js/sillyBackEndFile.js"); //specify which
 //property of exports we are interested in via 'require'
-//uses dot notation to pull out Calculator in the thingy property and store it in a new variable called Whatevs
+//store it in a new variable called Whatevs
 //Whatevs is uppercase bc it is an object constructor function
 
 $(document).ready(function(){
   $("#ping-pong-form").submit(function(event){
+    //for now, ping pong form will trigger instantiating a new Calculator object
+    //and have skin property hard-coded to hot pink
     event.preventDefault();
 
     var newNumber = $("#number").val();
     var simpleCalculator = new Whatevs("hot pink");
+    //need to create a Calculator object here so that the pingPong **method** can be called on it
     var results = simpleCalculator.pingPong(newNumber);
+    //passed newNumber variable into pingPong method = array of output
     results.forEach(function(element) {
       $("#solution").append("<li>" + element + "</li>");
     });
--- js/sillyBackEndFile.js
+++ js/sillyBackEndFile.js
@@ -1,10 +1,15 @@
 //backend
 
+console.log("hello?")
+
 function Calculator(skinName) {
   this.skin = skinName;
+  //property is named skin
+  //eventually, users will create a new object (instance) of Calculator every time they choose a new skin (i.e., "hot pink" or "retro green")
 }
 
 Calculator.prototype.pingPong = function(number){
+  //method that can be called on new instances of the Calculator constructor
   var output = [];
   for (var i = 0; i < number; i++); {
     if (i % 15 === 0) {
@@ -20,6 +25,8 @@ Calculator.prototype.pingPong = function(number){
   return output;
 }
 
-exports.thingy = Calculator; //here is a property on the global object
-//called thingy
-//this Calculator blueprint is now converted into a MODULE
+module.exports = Calculator;
+//this is how you store the Calculator declaration into a module
+//(above is in browserify format "module.exports", different from epicodus lesson)
+//the object Calculator constructor function is now converted into a MODULE
+//module is a big ol' gian JS global object
